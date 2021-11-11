29f154946f34697f8678dca95335354442889297
Foxxx48 <foxxdim@mail.ru>
2021-11-11 12:42:31 +0300

    11.11.21

--- src/main/java/com/fox/spring/mvc_hibernate_aop/entity/controller/MyController.java
+++ src/main/java/com/fox/spring/mvc_hibernate_aop/entity/controller/MyController.java
@@ -2,9 +2,12 @@ package com.fox.spring.mvc_hibernate_aop.entity.controller;
 
 import com.fox.spring.mvc_hibernate_aop.entity.Employee;
 import com.fox.spring.mvc_hibernate_aop.entity.dao.EmployeeDAO;
+import com.fox.spring.mvc_hibernate_aop.entity.srevice.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,16 +16,16 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @RequestMapping(name = "/")
     public String showAllEmployees(Model model) {
 
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
         return "all-employees";
     }
-
+    
     @RequestMapping(name = "/addNewEmployee")
     public String addNewEmployee(Model model) {
 
@@ -31,4 +34,15 @@ public class MyController {
 
         return "employee-info";
     }
+
+    @RequestMapping(name = "/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+
+        employeeService.saveEmployee(employee);
+
+        final var s = "redirect:/";
+        return s;
+    }
 }
+
